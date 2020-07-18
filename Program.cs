390838f85319e0182cf06ed6653c18067d4e05ab
shabbar fatima <55195258+shabbarfatima@users.noreply.github.com>
2020-07-18 21:56:01 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HUFFMANTEST
+{
+   
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------------FILE COMPRESSION-----------------------------");
+            List<Node> Nodes;
+            
+            
+                Tree tree = new Tree();
+                Console.Write("Enter the path of the file: ");
+                String filename = Console.ReadLine();
+                //file = filename;
+                FileInfo f = new FileInfo(filename);
+                long s1 = f.Length;
+                Console.Write("FileSize: " + s1.ToString());
+                Console.Write("\n");
+                Nodes = tree.CalculateFrequency(filename);
+                Console.WriteLine("#Symbols   -   #Frequency"); 
+                tree.PrintFrequency(Nodes);
+                tree.setHuffcode("", Nodes[0]);
+                tree.Ascii("", Nodes[0]);
+                tree.Filewrite(Nodes[0]);
+                Console.WriteLine("\n\n");
+                Console.WriteLine("\n\n#Symbols    -    #Codes\n");
+            //tree.Filewrite(nodeList[0]);
+                tree.Printcode(Nodes[0]);
+                Console.WriteLine("\n\n");
+
+        }
+    }
+}
